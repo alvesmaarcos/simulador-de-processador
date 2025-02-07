2be46e416c70d9121d6a10daf71c35732a956685
alvesmaarcos <alvesmarcos.es@gmail.com>
2025-02-07 14:17:56 -0300

    git commit -m "inicializacao de variaveis da struct processador"

--- processador.c
+++ processador.c
@@ -15,8 +15,8 @@ int main(){
     for (int i = 0; i < 8; i++) {
         c.R[i] = 0b0;
     }
-    c.PC = 0b0;
-    c.IR = 0b0;
+    c.PC = 0;
+    c.IR = 0;
     c.Z = 0b0;
     c.S = 0b0;
     c.C = 0b0;
